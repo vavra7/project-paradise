747069bea94de668f25236f5bbad56520510a1b1
Tomáš Vavřinka <email@tomasvavrinka.cz>
2019-10-02 20:59:45 +0200

    test

--- gatsby-dev/src/components/menus/MobileBottomMenu.jsx
+++ gatsby-dev/src/components/menus/MobileBottomMenu.jsx
@@ -37,7 +37,7 @@ const MobileBottomMenu = () => {
 				</li>
 
 				<li>
-					<Link to="/">
+					<Link to="test">
 						<FontAwesomeIcon icon={faEnvelope} />
 						<span>Kontakt</span>
 					</Link>
