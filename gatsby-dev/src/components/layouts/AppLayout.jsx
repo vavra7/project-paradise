13e818eb6b6aad2a4ae496a649a691b7bc62ed99
Tomáš Vavřinka <email@tomasvavrinka.cz>
2019-10-03 20:53:53 +0200

    refactorign of stylesheets

--- gatsby-dev/src/components/layouts/AppLayout.jsx
+++ gatsby-dev/src/components/layouts/AppLayout.jsx
@@ -8,8 +8,12 @@ const AppLayout = ({ children }) => (
 		<div id="fixed-right-bar" className={scopedStyles.fixedRightBar}>
 			right bar
 		</div>
-		<div id="app-layout-content-wrapper">{children}</div>
-		<div id="fixed-bottom-bar" className={scopedStyles.fixedBottomBar}>
+
+		<div id="app-layout-content-wrapper" className={scopedStyles.contentWrapper}>
+			{children}
+		</div>
+
+		<div id="fixed-bottom-bar" className={`${scopedStyles.fixedBottomBar} shadow-t-3 line-t-2`}>
 			<MobileBottomMenu />
 		</div>
 	</div>
