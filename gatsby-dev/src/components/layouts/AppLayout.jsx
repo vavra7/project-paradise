9603dd2545fb58074d75716250e7f9109c1289df
Tomáš Vavřinka <email@tomasvavrinka.cz>
2019-10-06 11:54:16 +0200

    right bar WIP

--- gatsby-dev/src/components/layouts/AppLayout.jsx
+++ gatsby-dev/src/components/layouts/AppLayout.jsx
@@ -1,27 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import scopedStyles from './AppLayout.module.scss';
 
 import MobileBottomMenu from '../menus/MobileBottomMenu';
 
-const AppLayout = ({ children }) => (
-	<div id="app-layout">
-		<div id="app-layout-content-wrapper" className={scopedStyles.contentWrapper}>
-			{children}
-		</div>
+class AppLayout extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			openRightBar: false
+		};
 
-		<div id="fixed-right-bar" className={`${scopedStyles.fixedRightBar} p-fixed d-flex`}>
-			<div className={scopedStyles.rightBarHandlerContainer}>handler</div>
+		this.toggleRightBar = this.toggleRightBar.bind(this);
+	}
 
-			<div className={`${scopedStyles.rightBarContentContainer} fg-1`}>content</div>
-		</div>
+	toggleRightBar() {
+		if (!this.state.openRightBar) {
+			this.openRightBar();
+		} else {
+			this.closeRightBar();
+		}
+	}
 
-		<div
-			id="fixed-bottom-bar"
-			className={`${scopedStyles.fixedBottomBar} p-fixed d-flex jc-flex-end fd-column bg-white shadow-t-3 line-t-2`}
-		>
-			<MobileBottomMenu />
-		</div>
-	</div>
-);
+	openRightBar() {
+		console.log('open');
+		this.setState({ openRightBar: true });
+	}
+
+	closeRightBar() {
+		console.log('close');
+		this.setState({ openRightBar: false });
+	}
+
+	render() {
+		return (
+			<div id="app-layout">
+				<i className="icon-bars p-fixed ma-3" onClick={this.toggleRightBar}></i>
+
+				<div id="app-layout-content-wrapper" className={scopedStyles.contentWrapper}>
+					{this.props.children}
+				</div>
+
+				<div id="fixed-right-bar" className={`${scopedStyles.fixedRightBar} p-fixed d-flex`}>
+					<div className={`${scopedStyles.rightBarHandlerContainer} p-absolute`}></div>
+
+					<div className={`${scopedStyles.rightBarContentContainer} fg-1`}></div>
+				</div>
+
+				<div
+					id="fixed-bottom-bar"
+					className={`${scopedStyles.fixedBottomBar} p-fixed d-flex jc-flex-end fd-column bg-white shadow-t-3 line-t-2`}
+				>
+					<MobileBottomMenu />
+				</div>
+			</div>
+		);
+	}
+}
+
+AppLayout.propTypes = {
+	children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.element), PropTypes.element]).isRequired
+};
 
 export default AppLayout;
