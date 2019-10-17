7db2d55b3c348dfb7af5b5f0abb9c40bd2e6cde8
Tomáš Vavřinka <email@tomasvavrinka.cz>
2019-10-17 21:00:44 +0200

    test

--- gatsby-dev/src/components/menus/MobileBottomMenu.jsx
+++ gatsby-dev/src/components/menus/MobileBottomMenu.jsx
@@ -1,48 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import scopedStyles from './MobileBottomMenu.module.scss';
 import { Link } from 'gatsby';
 
-const MobileBottomMenu = () => {
-	return (
-		<nav id="mobile-bottom-menu">
-			<ul className={`${scopedStyles.menuWrapper} d-flex jc-space-around`}>
-				<li>
-					<Link to="/">
-						<i className="icon-home"></i>
-						<span>Domů</span>
-					</Link>
-				</li>
+class MobileBottomMenu extends Component {
+	render() {
+		return (
+			<nav id="mobile-bottom-menu">
+				<ul className={`${scopedStyles.menuWrapper} d-flex jc-space-around`}>
+					<li>
+						<Link to="/">
+							<i className="icon-home"></i>
+							<span>Domů</span>
+						</Link>
+					</li>
 
-				<li>
-					<Link to="/">
-						<i className="icon-user-friends"></i>
-						<span>O nás</span>
-					</Link>
-				</li>
+					<li>
+						<Link to="/">
+							<i className="icon-user-friends"></i>
+							<span>O nás</span>
+						</Link>
+					</li>
 
-				<li>
-					<Link to="/">
-						<i className="icon-umbrella-beach"></i>
-						<span>Dovolené</span>
-					</Link>
-				</li>
+					<li>
+						<Link to="/">
+							<i className="icon-umbrella-beach"></i>
+							<span>Dovolené</span>
+						</Link>
+					</li>
 
-				<li>
-					<Link to="/">
-						<i className="icon-shopping-cart"></i>
-						<span>eShop</span>
-					</Link>
-				</li>
+					<li>
+						<Link to="/">
+							<i className="icon-shopping-cart"></i>
+							<span>eShop</span>
+						</Link>
+					</li>
 
-				<li>
-					<Link to="/">
-						<i className="icon-envelope"></i>
-						<span>Kontakt</span>
-					</Link>
-				</li>
-			</ul>
-		</nav>
-	);
-};
+					<li>
+						<Link to="/">
+							<i className="icon-envelope"></i>
+							<span>Kontakt</span>
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		);
+	}
+}
 
 export default MobileBottomMenu;
