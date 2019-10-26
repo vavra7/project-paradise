ef0a285900ba28360fe8d8d784083009b9394ae0
Tomáš Vavřinka <email@tomasvavrinka.cz>
2019-10-26 19:50:51 +0200

    test

--- gatsby-dev/src/components/menus/MobileBottomMenu.jsx
+++ gatsby-dev/src/components/menus/MobileBottomMenu.jsx
@@ -9,46 +9,46 @@ class MobileBottomMenu extends Component {
 			<nav id="mobile-bottom-menu">
 				<ul className={`${scopedStyles.menuWrapper} d-flex jc-space-around`}>
 					<li>
-						{/* <RippleEffect> */}
+						<RippleEffect>
 						<Link to="/">
 							<i className="icon-home"></i>
 							<span>Domů</span>
 						</Link>
-						{/* </RippleEffect> */}
+						</RippleEffect>
 					</li>
 					<li>
-						{/* <RippleEffect> */}
+						<RippleEffect>
 						<Link to="/test">
 							<i className="icon-user-friends"></i>
 							<span>O nás</span>
 						</Link>
-						{/* </RippleEffect> */}
+						</RippleEffect>
 					</li>
 					<li>
-						{/* <RippleEffect> */}
+						<RippleEffect>
 						<Link to="/test2">
 							<i className="icon-umbrella-beach"></i>
 							<span>Dovolené</span>
 						</Link>
-						{/* </RippleEffect> */}
+						</RippleEffect>
 					</li>
 
 					<li>
-						{/* <RippleEffect> */}
+						<RippleEffect>
 						<Link to="/">
 							<i className="icon-shopping-cart"></i>
 							<span>eShop</span>
 						</Link>
-						{/* </RippleEffect> */}
+						</RippleEffect>
 					</li>
 
 					<li>
-						{/* <RippleEffect> */}
+						<RippleEffect>
 						<Link to="/">
 							<i className="icon-envelope"></i>
 							<span>Kontakt</span>
 						</Link>
-						{/* </RippleEffect> */}
+						</RippleEffect>
 					</li>
 				</ul>
 			</nav>
