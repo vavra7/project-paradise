c29f3c9c6a9b58ffcb51a7c4301ada9d03e13044
Tomáš Vavřinka <email@tomasvavrinka.cz>
2019-11-11 15:47:14 +0100

    fix template sourcing

---
+++ gatsby/src/components/layouts/CommonPostLayout.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class CommonPostLayout extends Component {
+	static propTypes = {
+		prop: PropTypes
+	};
+
+	render() {
+		return <div></div>;
+	}
+}
+
+export default CommonPostLayout;
