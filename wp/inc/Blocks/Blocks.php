54ec4fc8b1b3734eda579f8bf63661fba7d93f12
Tomáš Vavřinka <email@tomasvavrinka.cz>
2019-12-28 12:09:00 +0100

    included general wp block styles

--- wp/inc/Blocks/Blocks.php
+++ wp/inc/Blocks/Blocks.php
@@ -10,6 +10,7 @@ class Blocks extends Enqueue
 	{
 		add_action('enqueue_block_editor_assets', [$this, 'register_block_scripts']);
 		add_action('init', [$this, 'register_block_types']);
+		add_action('init', [$this, 'setup_blocks']);
 	}
 
 	/**
@@ -20,5 +21,15 @@ class Blocks extends Enqueue
 		register_block_type('namespace/slug-test', [
 			'editor_script' => self::$BLOCK_SCRIPTS
 		]);
+	}
+
+	// TODO: zkontrolovat funkcionalitu, aktuálně nefuguje a má být opraveno ve verzi wordpressu 5.4
+	// https://developer.wordpress.org/block-editor/developers/themes/theme-support/#responsive-embedded-content
+	/**
+	 * general blocks setup
+	 */
+	public function setup_blocks(): void
+	{
+		add_theme_support('responsive-embeds');
 	}
 }
